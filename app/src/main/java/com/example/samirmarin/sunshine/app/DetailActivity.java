a05d9e8c6550ad827a4ed4614a16ad75af7c2a45
Samir Marin <sam070@mail.usask.ca>
2015-05-29 00:48:06 -0700

    delatiled view shows.. rough

--- app/src/main/java/com/example/samirmarin/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/samirmarin/sunshine/app/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.samirmarin.sunshine.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -11,7 +13,15 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        //setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        String weatherData = intent.getStringExtra(intent.EXTRA_TEXT);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(weatherData);
+
+        setContentView(textView);
 
     }
 
